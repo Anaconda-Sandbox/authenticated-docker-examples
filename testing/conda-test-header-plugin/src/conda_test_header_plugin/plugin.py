d8f6a6055b9ddcff5009e7aeae891440e5b15e73
mattkram <mkramer@anaconda.com>
2025-12-17 14:31:39 -0600

    Revert "feat: Load test header from ANACONDA_TEST_HEADER environment variable"
    This reverts commit 73cc962dadb0726b63d391d9d7e8d1966114f27a.

--- testing/conda-test-header-plugin/src/conda_test_header_plugin/plugin.py
+++ testing/conda-test-header-plugin/src/conda_test_header_plugin/plugin.py
@@ -1,13 +1,10 @@
-import os
-
 from conda import plugins
 
 
 @plugins.hookimpl
 def conda_session_headers(host: str):
     """Add a special header to mock an unregistered user."""
-    if header_value := os.getenv("ANACONDA_TEST_HEADER"):
-        yield plugins.CondaRequestHeader(
-            name="X-Anaconda-Testing",
-            value=header_value,
-        )
+    yield plugins.CondaRequestHeader(
+        name="X-Anaconda-Testing",
+        value="user:unregistered",
+    )
