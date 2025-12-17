73cc962dadb0726b63d391d9d7e8d1966114f27a
mattkram <mkramer@anaconda.com>
2025-12-17 12:48:06 -0600

    feat: Load test header from ANACONDA_TEST_HEADER environment variable

--- testing/conda-test-header-plugin/src/conda_test_header_plugin/plugin.py
+++ testing/conda-test-header-plugin/src/conda_test_header_plugin/plugin.py
@@ -1,10 +1,13 @@
+import os
+
 from conda import plugins
 
 
 @plugins.hookimpl
 def conda_session_headers(host: str):
     """Add a special header to mock an unregistered user."""
-    yield plugins.CondaRequestHeader(
-        name="X-Anaconda-Testing",
-        value="user:unregistered",
-    )
+    if header_value := os.getenv("ANACONDA_TEST_HEADER"):
+        yield plugins.CondaRequestHeader(
+            name="X-Anaconda-Testing",
+            value=header_value,
+        )
